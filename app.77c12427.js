8d362af011b5d863c412ffa3706009c550fbaa6f
Arnulfo Antonio Miramontes Beas <arnulfo_amb182@hotmail.com>
2024-06-02 22:04:51 -0600

    Updates

--- app.77c12427.js
+++ app.77c12427.js
@@ -192,7 +192,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52875" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59454" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
